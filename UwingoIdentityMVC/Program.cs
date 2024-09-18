570374162097862ba18498deb84c4927eeb372e2
kdrkrc19 <kadircankaraca01@gmail.com>
2024-09-18 11:02:14 +0300

    Add project files.

---
+++ UwingoIdentityMVC/Program.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using UwingoIdentityMVC;
+using UwingoIdentityMVC.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// JWT Configuration
+builder.Services.ConfigureJWT(builder.Configuration);
+
+builder.Services.ConfigureApplicationCookie();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+})
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/Authentication/Login";
+                    options.LogoutPath = "/Authentication/Logout";
+                    options.AccessDeniedPath = "/Authentication/Login";
+                });
+
+// Initialize HttpClient base address
+builder.Services.InitializeClientBaseAddress(builder.Configuration);
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+builder.Services.AddHttpContextAccessor();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseStatusCodePagesWithReExecute("/Error/{0}");
+
+app.UseSession();
+
+// Add Authentication middleware
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
