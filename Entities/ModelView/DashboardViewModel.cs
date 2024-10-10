a3822fdd711ddc3276a4a71f9aa6d497c2668cf0
kdrkrc19 <kadircankaraca01@gmail.com>
2024-10-11 01:02:06 +0300

    dto,LogoUpdates

--- Entities/ModelView/DashboardViewModel.cs
+++ Entities/ModelView/DashboardViewModel.cs
@@ -11,7 +11,6 @@ namespace Entity.ModelsDto
         public int CompanyCount { get; set; }
         public int CompanyApplicationCount { get; set; }
         public int ApplicationCount { get; set; }
-        public int PermissionCount { get; set; }
         public int RoleCount { get; set; }
         public int TenantCount { get; set; }
         public int UserCount { get; set; }
